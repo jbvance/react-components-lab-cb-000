b61ecc0c7a72c2438136be291e39b5e52220ae21
Jason Vance <jbvance@gmail.com>
2017-12-23 14:10:15 +0000

    automatically backed up by learn

--- src/index.js
+++ src/index.js
@@ -20,7 +20,7 @@ class OlderCoaster {
 class InFrontOfYou {
   render() {
     <div>
-  <p>You shouldn't look too far.</p>
+  <p>You shouldnt look too far.</p>
   <p>Sometimes, the solution is right in front of you.</p>
 </div>
   }
